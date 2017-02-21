3ba94d70479abbd0b606cdeeb7eec5a99eca46af
Tushar Agarwal <tusharag171@gmail.com>
2017-02-21 17:03:45 -0500

    database files

---
+++ mantessa_db/generate_db2.py
@@ -0,0 +1,29 @@
+import MySQLdb
+import pandas as pd
+import datetime
+
+reader = pd.read_csv('20170207.csv',chunksize=10000)
+#df2 = df.head()
+
+
+conn = MySQLdb.connect(host= "localhost",
+                  user="root",
+                  passwd="",
+                  db="mantessa_db")
+dbcursor = conn.cursor()
+start = datetime.datetime.now()
+stmt = "INSERT INTO mantessa VALUES (INET_ATON(%s),%s,%s,,CURRENT_TIMESTAMP,1,0)"
+try:
+	for df in reader:
+		df=df.values.tolist()
+		l = len(df)
+		print l
+		dbcursor.executemany(stmt,df)
+			
+	conn.commit()
+except Exception as e:
+	print "Oops!! Something went wrong "+str(e) 
+	conn.rollback()
+end = datetime.datetime.now()
+print end-start
+conn.close()
---
+++ mantessa_db/update_db.py
@@ -0,0 +1,44 @@
+import MySQLdb
+import pandas as pd
+import datetime
+
+f_name = '20170208'
+reader = pd.read_csv(f_name+'.csv',chunksize=10000)
+#df2 = df.head()
+
+
+conn = MySQLdb.connect(host= "localhost",
+                  user="root",
+                  passwd="",
+                  db="mantessa_db")
+dbcursor = conn.cursor()
+
+
+try:
+	dbcursor.execute("alter table mantessa add d_"+f_name+" BOOLEAN")
+	conn.commit()
+except Exception as e:
+	print "Oops!! Something went wrong "+str(e)
+	conn.rollback()
+
+dbcursor = conn.cursor()
+
+
+start = datetime.datetime.now()
+
+try:
+	for df in reader:
+		df=df.values.tolist()
+		for item in df:
+			#print item[1], item[2], item[3]
+			dbcursor.callproc('update_mantessa',('d_'+f_name,item[1],item[2],item[3]))
+			result = dbcursor.fetchall()
+						
+	conn.commit()
+except Exception as e:
+	print "Oops!! Something went wrong "+str(e) 
+	conn.rollback()
+end = datetime.datetime.now()
+
+print end-start
+conn.close()
