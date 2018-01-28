7566a38f04e2ba320ca0d109ba933b7bf38276b1
Joshua Zweig <jmz2135@columbia.edu>
2018-01-27 20:39:38 -0500

    Tweaks for scanning

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,7 @@
 # Python pipeline to execute all 
 # functionality for MANTESSA pipeline 
 
+import subprocess
 from subprocess import call 
 import datetime 
 
@@ -15,9 +16,9 @@ def main():
   run_scan()
 
   # Run scoring algorithm
-  score()
+  # score()
 
-  update_map()
+  # update_map()
 
 
 # Run a zmap scan
@@ -26,9 +27,12 @@ def main():
 #     2. Kick off job to ru nthe scoring algorithm / create list of IPs to map
 def run_scan():
   # Execute zmap scan from whitelist
-  instant = datetime.datetime.now.strftime("%Y%m%dT%H_%M_%S")
+  instant = datetime.datetime.now().strftime("%Y%m%dT%H_%M_%S")
   dt_file = "d_" + instant
-  subprocess.call("sudo zmap -B 15M --probe-module=icmp_echoscan --whitelist-file=\"" + WHITELIST_FILE + "\" -o " + dt_file)
+  filestring = "sudo zmap -B 15M --probe-module=icmp_echoscan --whitelist-file=\"" + WHITELIST_FILE + "\" -o " + dt_file
+  print(filestring) 
+ 
+  subprocess.call("touch " + dt_file +" && sudo zmap -B 15M --probe-module=icmp_echoscan --whitelist-file=\"" + WHITELIST_FILE + "\" -o " + dt_file)
   
   # Fork to insert into DB 
 
@@ -40,13 +44,13 @@ def run_scan():
 # When the scores are computed do 2 things
 #     1. Send the top N IPs to the mapping job
 #     2. Write the list of IPs to scan next time back to a file
-def score():
+#def score():
 
 
 
 # If the map is not runnig, run it
 # Write a new datafile for the map to read from 
-def update_map():
+#def update_map():
 
 
 main() 
